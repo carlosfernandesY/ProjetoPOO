a20e0734dc5cbda2b6932eb9d01b740781b6fd13
viniciusfga <viniciusfg.araujo@gmail.com>
2026-05-25 21:53:10 -0300

    restore: Implementação BarbeiroDAO.java

--- src/main/java/org/ifgoiano/barbearia/dao/BarbeiroDAO.java
+++ src/main/java/org/ifgoiano/barbearia/dao/BarbeiroDAO.java
@@ -1,29 +1,81 @@
 package org.ifgoiano.barbearia.dao;
 
-
+import org.ifgoiano.barbearia.connection.ConnectionFactory;
 import org.ifgoiano.barbearia.dao.interfaceClass.EntidadeDAO;
 import org.ifgoiano.barbearia.model.Barbeiro;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class BarbeiroDAO implements EntidadeDAO<Barbeiro> {
 
+    Connection connection;
+
+    public BarbeiroDAO(){
+        this.connection = (Connection) new ConnectionFactory().getConnection();
+    }
     @Override
     public void create(Barbeiro object) {
+        String sql = "INSERT INTO Barbeiro (nome) VALUES (?);";
 
+        try(PreparedStatement preparedStatement = this.connection.prepareStatement(sql)){
+            preparedStatement.setString(1,object.getNome());
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public Barbeiro readById(Integer id) {
-        return null;
+        String sql = "SELECT * FROM Barbeiro wHERE idBarbeiro = ?;";
+        Barbeiro barbeiro = new Barbeiro();
+
+        try(PreparedStatement preparedStatement = this.connection.prepareStatement(sql)){
+            preparedStatement.setInt(1, id);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            rs.next();
+            barbeiro = new Barbeiro();
+            barbeiro.setIdBarbeiro(rs.getInt("idBarbeiro"));
+            barbeiro.setNome(rs.getString("nome"));
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return barbeiro;
     }
 
     @Override
-    public boolean deleteById(Barbeiro barbeiro) {
-        return false;
+    public boolean deleteById(Barbeiro object) {
+        String sql = "DELETE FROM Barbeiro wHERE idBarbeiro = ?;";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement .setInt(1, object.getIdBarbeiro());
+            preparedStatement.execute();
+            return true;
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+
     }
 
     @Override
     public void updateById(Barbeiro object) {
+        String sql = "UPDATE Barbeiro SET nome = ? wHERE idBarbeiro = ?;";
 
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setString(1, object.getNome());
+            preparedStatement.setInt(2, object.getIdBarbeiro());
+            preparedStatement.execute();
 
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
+
+
 }
