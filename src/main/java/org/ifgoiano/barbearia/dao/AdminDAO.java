eb297a9a0a62cb667ad05a0f1c11b10b6082f6cb
carlosfernandesY <ferreirafernandescarlosroberto@gmail.com>
2026-05-28 02:16:06 -0300

    Senha para os caras

--- src/main/java/org/ifgoiano/barbearia/dao/AdminDAO.java
+++ src/main/java/org/ifgoiano/barbearia/dao/AdminDAO.java
@@ -1,27 +1,85 @@
 package org.ifgoiano.barbearia.dao;
 
+import org.ifgoiano.barbearia.dao.interfaceClass.EntidadeDAO;
 import org.ifgoiano.barbearia.model.Admin;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.List;
 
-public class AdminDAO {
-    Connection  connection;
+
+public class AdminDAO implements EntidadeDAO<Admin> {
+    Connection connection;
+
     public AdminDAO(Connection connection) {
         this.connection = connection;
     }
+
+    @Override
     public boolean create(Admin admin) {
         String sql = "INSERT INTO admin (login,senha) VALUES (?,?)";
-        try(PreparedStatement preparedStatement  = connection.prepareStatement(sql)) {
-            preparedStatement.setString(1,admin.getLogin());
-            preparedStatement.setString(2,admin.getSenha());
-            repreparedStatement.executeUpdate();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, admin.getLogin());
+            preparedStatement.setString(2, admin.getSenha());
+            return preparedStatement.executeUpdate() > 0;
 
 
-        }catch (SQLException e){
-            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
+        return false;
+    }
+
+    @Override
+    public Admin readById(Integer id) {
+        String sql = "SELECT * FROM admin WHERE idAdmin = ?;";
+        Admin admin = new Admin();
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            admin.setIdAdmin(resultSet.getInt("idAdmin"));
+            admin.setLogin(resultSet.getString("login"));
+            admin.setSenha(resultSet.getString("senha"));
+            return admin;
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public boolean updateById(Admin object) {
+        String sql = "UPDATE admin SET login=?, senha=? WHERE idAdmin=?;";
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, object.getLogin());
+            preparedStatement.setString(2, object.getSenha());
+            preparedStatement.setInt(3, object.getIdAdmin());
+            return preparedStatement.executeUpdate() > 0 ;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(Admin object) {
+        String sql = "DELETE FROM admin WHERE idAdmin=?;";
+        try(PreparedStatement  preparedStatement = this.connection.prepareStatement(sql)){
+            preparedStatement.setInt(1,object.getIdAdmin());
+           return  preparedStatement.execute();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+    @Override
+    public List<Admin> readAll() {
+        return List.of();
     }
 }
+
+
